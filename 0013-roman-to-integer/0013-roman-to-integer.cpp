757435e742717479632aeafea615cee3bc8d021a
Vikas Kashyap <221534262+VkasKashyap@users.noreply.github.com>
2026-07-17 15:22:47 +0530

    Time: 0 ms (100%), Space: 9.9 MB (99.41%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int value(char r){
+        switch(r){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+    int romanToInt(string &s) {
+        int n = s.length();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            int s1 = value(s[i]);
+            if(i+1 < n){
+                int s2 = value(s[i+1]);
+                if(s1 < s2){
+                    sum -= s1;
+                }
+                else{
+                    sum += s1;
+                }
+            }
+            else{
+                sum +=s1;
+            }
+        }
+        return sum;
+    }
+};
