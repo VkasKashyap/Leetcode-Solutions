9df2077ec7e06b1ece54541011691f40b14e79f5
Vikas Kashyap <221534262+VkasKashyap@users.noreply.github.com>
2026-07-18 11:42:10 +0530

    Time: 0 ms (100%), Space: 10.4 MB (47.44%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        vector<int> nums = {0, 0};
-        for(int i=0;i<moves.size();i++){
-            if(moves[i] == 'U'){
-                nums[1] += 1;
-            }
-            else if(moves[i] == 'D'){
-                nums[1] -= 1;
-            }
-            else if(moves[i] == 'L'){
-                nums[0] += 1;
-            }
-            else if(moves[i] == 'R'){
-                nums[0] -= 1;
-            }
+        int x = 0, y = 0;
+        for (char move : moves) {
+            if (move == 'U') y++;
+            else if (move == 'D') y--;
+            else if (move == 'L') x--;
+            else if (move == 'R') x++;
         }
-    	if(nums[0] == 0 && nums[1] == 0)    return true;
-        return false;
+        return x == 0 && y == 0;
     }
 };
